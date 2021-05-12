bcae84f3169ef73dcb786bcea96e7ef8efd15f37
Courtney Amos <c.amos@shaw.ca>
2021-05-11 20:06:40 -0700

    import Appointment component into Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,55 @@
 import React, { useState } from "react";
 
 import "components/Application.scss";
-import DayList from "components/DayList.js";
+import DayList from "components/DayList";
+import Appointment from "components/Appointment/index"
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Kiara Husky",
+      interviewer: {
+        id: 2, 
+        name: "Tori Malcolm", 
+        avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Sully Bear",
+      interviewer: { 
+        id: 3, 
+        name: "Mildred Nazir", 
+        avatar: "https://i.imgur.com/T2WwVfS.png" 
+      }
+    }
+  }
+];
 
 const days = [
   {
@@ -39,7 +87,13 @@ export default function Application() {
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs"/>
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointmentItem => (
+          <Appointment 
+            key={appointmentItem.id}
+            {...appointmentItem}
+          />
+        ))}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
