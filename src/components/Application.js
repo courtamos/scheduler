f05a779b6ac600b45fd57d8dbfea813bdb17b7fc
Courtney Amos <c.amos@shaw.ca>
2021-05-12 21:12:35 -0700

    use axios to retrieve the appointments data and set it as state in the Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,72 +3,33 @@ import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index"
+import { getAppointmentsForDay } from "helpers/selectors.js";
 
 const axios = require('axios');
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Kiara Husky",
-      interviewer: {
-        id: 2, 
-        name: "Tori Malcolm", 
-        avatar: "https://i.imgur.com/Nmx0Qxo.png"
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "3pm",
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Sully Bear",
-      interviewer: { 
-        id: 3, 
-        name: "Mildred Nazir", 
-        avatar: "https://i.imgur.com/T2WwVfS.png" 
-      }
-    }
-  }
-];
-
 
 export default function Application() {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // appointments: {}
+    appointments: {}
   })
 
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
   const setDay = day => setState({ ...state, day });
   
   useEffect(() => {
-    axios.get('/api/days')
-    .then(({ data }) => {
-      setState(prev => ({ ...prev, days: data }));
-      })
-  }, []);
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments')
+    ])
+    .then(all => {
+      setState(prev => ({ 
+        ...prev, 
+        days: all[0].data, 
+        appointments: all[1].data 
+      }))
+    })
+  }, [])
 
   return (
     <main className="layout">
@@ -85,7 +46,7 @@ export default function Application() {
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs"/>
       </section>
       <section className="schedule">
-        {appointments.map(appointmentItem => (
+        {dailyAppointments.map(appointmentItem => (
           <Appointment 
             key={appointmentItem.id}
             {...appointmentItem}
