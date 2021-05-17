24cec57240240d2451626b757b492d734702587d
Courtney Amos <c.amos@shaw.ca>
2021-05-17 12:46:55 -0700

    type checking with PropTypes in InterviewerList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,14 @@
 import React from "react";
+import PropTypes from 'prop-types';
+
 import "components/InterviewerList.scss"
 import InterviewerListItem from "components/InterviewerListItem.js";
 
 export default function InterviewerList ({ value, interviewers, onChange }) {
+  InterviewerList.propTypes = {
+    interviewers: PropTypes.array.isRequired
+  };
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
