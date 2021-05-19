44d9e8b73239623a2b91b864d7232208f6203053
Courtney Amos <c.amos@shaw.ca>
2021-05-19 15:41:14 -0700

    write cypress test for booking an interview

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,13 +1,13 @@
 describe("Navigation", () => {
   it("should visit root", () => {
-    cy.visit('/');
+    cy.visit("/");
   });
 
   it("should navigate to Tuesday", () => {
-    cy.visit('/');
+    cy.visit("/");
 
-    cy.contains('[data-testid=day]', 'Tuesday')
+    cy.contains("[data-testid=day]", "Tuesday")
       .click()
-      .should('have.class', 'day-list__item--selected')
+      .should("have.class", "day-list__item--selected")
   });
 });
